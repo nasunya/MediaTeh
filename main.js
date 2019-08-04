b7cb2a9c9d328b94b2de06a6f95acf617e0324d1
unknown <tischina-nast2012@yandex.ru>
2019-08-04 13:13:58 +0300

    Верстка уведомлений

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 alert("Текст для ввода на экран")
+
+function notifyMe() {
+    // Проверка поддержки браузером уведомлений
+    if (!("Notification" in window)) {
+    alert("Этот браузер не поддерживает уведомления на рабочем столе");
+    }
+    
+    // Проверка разрешения на отправку уведомлений
+    else if (Notification.permission === "granted") {
+    // Если разрешено, то создаем уведомление
+    var notification = new Notification("Hi there!");
+    }
+    
+    // В противном случае, запрашиваем разрешение
+    else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+    // Если пользователь разрешил, то создаем уведомление 
+    if (permission === "granted") {
+    var notification = new Notification("Hi there!");
+    }
+    });
+    }
+    }
+
+    notifyMe()
