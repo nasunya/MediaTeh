a06dc60bb1cda80692bd0a95e00c053038657988
unknown <tischina-nast2012@yandex.ru>
2019-08-04 22:51:53 +0300

    Переход на гугл

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
 alert("Текст для ввода на экран")
 
 function notifyMe() {
-  // Проверка поддержки браузером уведомлений
   if (!("Notification" in window)) {
   alert("Этот браузер не поддерживает уведомления на рабочем столе");
   }
-  
-  // Проверка разрешения на отправку уведомлений
   else if (Notification.permission === "granted") {
-  // Если разрешено, то создаем уведомление
   var notification = new Notification("Привет! Теперь ты будешь в курсе всех новостей :)");
   }
-  
-  // В противном случае, запрашиваем разрешение
   else if (Notification.permission !== 'denied') {
   Notification.requestPermission(function (permission) {
-  // Если пользователь разрешил, то создаем уведомление 
   if (permission === "granted") {
   var notification = new Notification("Привет! Теперь ты будешь в курсе всех новостей :)");
   }
   });
   }
   }
-
   notifyMe()
 
   elem.onclick = function() {
-      console.log(window.location.href);
-      window.location.assign('https://www.google.ru/');
+    var link                   = document.location.search.substr(5);
+        document.location.href = link;
   };
